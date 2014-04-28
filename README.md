33301370c87816676593c7b73057bca47d38fa77
Hyoo Lim <hyoolim@gmail.com>
2014-04-28 10:18:03 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,71 @@
+# Introduction
+
+Standard set of Grunt configuration for Brightspot projects using the following:
+
+- [Brightspot](http://www.brightspotcms.com/)
+- [Grunt](http://gruntjs.com/)
+- [LESS](http://lesscss.org/)
+- [RequireJS](http://requirejs.org/)
+  - [almond](https://github.com/jrburke/almond)
+  - [r.js](http://requirejs.org/docs/optimization.html)
+
+# Usage
+
+`package.json` (for Grunt dependencies):
+
+    {
+      "name": "foo",
+      "devDependencies": {
+        "bsp-grunt": "^1"
+      }
+    }
+
+`Gruntfile.js` (single minified CSS file from LESS and single static JS file using RequireJS and almond):
+
+    module.exports = function(grunt) {
+      require('bsp-grunt')(grunt, {
+        bsp: {
+          styles: {
+            dir: "assets/styles",
+            less: "foo.less"
+          },
+          scripts: {
+            dir: "assets/scripts",
+            rjsMain: "foo"
+          }
+        }
+      });
+    };
+
+`Gruntfile.js` (multiple minified CSS from LESS and multiple JS modules using RequireJS):
+
+    module.exports = function(grunt) {
+      require('bsp-grunt')(grunt, {
+        bsp: {
+          styles: {
+            dir: "assets/styles",
+            less: [ "foo.less", "bar.less" ]
+          },
+          scripts: {
+            dir: "assets/scripts",
+            rjsModules: [
+                {
+                    name: "foo"
+                },
+                {
+                    name: "bar"
+                }
+            ]
+          }
+        }
+      });
+    };
+
+`bower.json` (for project-specific dependencies):
+
+    {
+      "name": "foo",
+      "dependencies": {
+        "jquery": "^1.11"
+      }
+    }
---
+++ bsp-grunt.js
@@ -0,0 +1,134 @@
+module.exports = function(grunt, config) {
+    grunt.initConfig(require('extend')(true, { }, {
+        bsp: {
+            maven: {
+                srcDir: 'src/main/webapp',
+                targetDir: 'target',
+                destDir: '<%= bsp.maven.targetDir %>/' + grunt.option('bsp-maven-build-finalName')
+            },
+
+            bower: {
+                buildDir: '<%= bsp.maven.targetDir %>/bsp-grunt-bower'
+            },
+
+            styles: {
+                dir: '',
+                less: [ ],
+                ext: '.min.css',
+                srcDir: '<%= bsp.maven.srcDir %>/<%= bsp.styles.dir %>',
+                minDir: '<%= bsp.maven.destDir %>/<%= bsp.styles.dir %>'
+            },
+
+            scripts: {
+                dir: '',
+                srcDir: '<%= bsp.maven.srcDir %>/<%= bsp.scripts.dir %>',
+                devDir: '<%= bsp.maven.destDir %>/<%= bsp.scripts.dir %>',
+                minDir: '<%= bsp.scripts.devDir %>.min'
+            }
+        },
+
+        bower: {
+            install: {
+                options: {
+                    cleanBowerDir: true,
+                    targetDir: '<%= bsp.bower.buildDir %>'
+                }
+            }
+        },
+
+        copy: {
+            almond: {
+                files: {
+                    '<%= bsp.scripts.devDir %>/almond.js': 'node_modules/almond/almond.js'
+                }
+            },
+
+            requirejs: {
+                files: {
+                    '<%= bsp.scripts.devDir %>/require.js': 'node_modules/requirejs/require.js'
+                }
+            },
+
+            bower: {
+                files: [
+                    {
+                        cwd: '<%= bower.install.options.targetDir %>',
+                        dest: '<%= bsp.scripts.devDir %>',
+                        expand: true,
+                        filter: 'isFile',
+                        flatten: true,
+                        src: '**'
+                    }
+                ]
+            },
+
+            scripts: {
+                files: [
+                    {
+                        cwd: '<%= bsp.scripts.srcDir %>',
+                        dest: '<%= bsp.scripts.devDir %>',
+                        expand: true,
+                        src: '**'
+                    }
+                ]
+            }
+        },
+
+        less: {
+            compile: {
+                files: [
+                    {
+                        cwd: '<%= bsp.styles.srcDir %>',
+                        dest: '<%= bsp.styles.minDir %>',
+                        expand: true,
+                        ext: '<%= bsp.styles.ext %>',
+                        extDot: 'last',
+                        src: '<%= bsp.styles.less %>'
+                    }
+                ],
+
+                options: {
+                    cleancss: true,
+                    compress: true
+                }
+            }
+        },
+
+        requirejs: {
+            'static': {
+                options: {
+                    baseUrl: '<%= bsp.scripts.devDir %>',
+                    include: 'almond',
+                    name: '<%= bsp.scripts.rjsMain %>',
+                    optimize: 'uglify2',
+                    out: '<%= bsp.scripts.minDir %>/<%= bsp.scripts.rjsMain %>.js'
+                }
+            },
+
+            dynamic: {
+                options: {
+                    baseUrl: '<%= bsp.scripts.devDir %>',
+                    dir: '<%= bsp.scripts.minDir %>',
+                    modules: '<%= bsp.scripts.rjsModules %>',
+                    optimize: 'uglify2'
+                }
+            }
+        }
+    }, (config || { })));
+
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+
+    var hasRjsMain = !!grunt.config('bsp.scripts.rjsMain');
+
+    grunt.registerTask('default', [
+        'less:compile',
+        'bower:install',
+        'copy:' + (hasRjsMain ? 'almond' : 'requirejs'),
+        'copy:bower',
+        'copy:scripts',
+        'requirejs:' + (hasRjsMain ? 'static' : 'dynamic')
+    ]);
+};

4c329e6f5b895ea483a6b429c526e94b5bfb8be6
Rusty Tanton <rusty@perfectsensedigital.com>
2015-08-22 19:30:46 -0400

    added new config-dest task which reads from pom.xml

---
+++ tasks/config-dest.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+var parser = require('xml2json');
+
+module.exports = function(grunt) {
+	grunt.registerTask('bsp-config-dest', 'Configures destination directory', function() {
+		if (!grunt.config('bsp.maven.destDir')) {
+			var pom = grunt.config('bsp.maven.pom');
+			var buildName;
+			var xml;
+			if (fs.existsSync(pom)) {
+				xml = parser.toJson( fs.readFileSync(pom), { object: true } );
+				buildName = xml.project.artifactId + '-' + xml.project.version;
+				grunt.config('bsp.maven.destDir', '<%= bsp.maven.targetDir %>/' + buildName);
+				console.log( grunt.config('bsp.maven.destDir') );
+			} else {
+				grunt.fail.fatal(pom + ' not found, is required to configure the target directory');
+			}
+		}
+	});
+};

62c0e79c5b9f38d754b80f52985b011feffd127b
Hyoo Lim <hyoolim@gmail.com>
2015-11-06 09:17:10 -0500

    Improves create-binaries task to do so off global node if local one isn't available for consistency.

--- tasks/create-binaries.js
+++ tasks/create-binaries.js
@@ -3,12 +3,6 @@ var path = require('path');
 
 module.exports = function(grunt) {
 	grunt.registerTask('create-binaries', 'Creates helper binaries.', function() {
-        var realNodePath = path.join('node', 'node');
-
-        if (!fs.existsSync(realNodePath)) {
-            return;
-        }
-
         var targetPath = grunt.config('bsp.maven.targetDir');
         
 		if (!targetPath) {
@@ -16,21 +10,33 @@ module.exports = function(grunt) {
         }
 
         var binDirPath = path.join(targetPath, 'bin');
-        realNodePath = path.relative(binDirPath, realNodePath);
+        var realNodePath = path.join('node', 'node');
+        var shRealNodePath;
+        var cmdRealNodePath; 
+
+        if (fs.existsSync(realNodePath)) {
+            realNodePath = path.relative(binDirPath, realNodePath);
+            shRealNodePath = '"$(dirname $0)/' + realNodePath + '"';
+            cmdRealNodePath = '%~dp0' + realNodePath;
+
+        } else {
+            shRealNodePath = cmdRealNodePath = 'node';
+        }
+
         var nodeBinPath = path.join(binDirPath, 'node'); 
 
-        grunt.file.write(nodeBinPath, '#/bin/sh\n"$(dirname $0)/' + realNodePath + '" "$@"');
+        grunt.file.write(nodeBinPath, '#/bin/sh\n' + shRealNodePath + ' "$@"');
         fs.chmodSync(nodeBinPath, 0755);
-        grunt.file.write(nodeBinPath + '.cmd', '@echo off\n%~dp0' + realNodePath + ' %*\n@echo on');
+        grunt.file.write(nodeBinPath + '.cmd', '@echo off\n' + cmdRealNodePath + ' %*\n@echo on');
         grunt.log.writeln('Created binary: node');
 
         grunt.file.expand(path.join(__dirname, '..', 'node_modules/*/bin/*')).forEach(function (namePath) {
             var name = path.basename(namePath);
             var binPath = path.join(binDirPath, name);
 
-            grunt.file.write(binPath, '#/bin/sh\n$(dirname $0)/' + realNodePath + ' $(dirname $0)/' + path.relative(binDirPath, namePath) + ' "$@"');
+            grunt.file.write(binPath, '#/bin/sh\n' + shRealNodePath + ' $(dirname $0)/' + path.relative(binDirPath, namePath) + ' "$@"');
             fs.chmodSync(binPath, 0755);
-            grunt.file.write(binPath + '.cmd', '@echo off\n%~dp0' + realNodePath + ' %~dp0' + path.relative(binDirPath, namePath) + ' %*\n@echo on');
+            grunt.file.write(binPath + '.cmd', '@echo off\n' + cmdRealNodePath + ' %~dp0' + path.relative(binDirPath, namePath) + ' %*\n@echo on');
             grunt.log.writeln('Created binary: ' + name);
         });
 	});

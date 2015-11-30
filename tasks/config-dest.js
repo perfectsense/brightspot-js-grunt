251abe8145da38f4d65d543ae9569c3d20cf686c
Hyoo Lim <hyoolim@gmail.com>
2015-11-30 14:00:18 -0500

    Changes to using xml2js instead of xml2json as not to depend on node-expat.

--- tasks/config-dest.js
+++ tasks/config-dest.js
@@ -1,17 +1,22 @@
 var fs = require('fs');
-var parser = require('xml2json');
+var parseString = require('xml2js').parseString;
 
 module.exports = function(grunt) {
 	grunt.registerTask('bsp-config-dest', 'Configures destination directory', function() {
 		if (!grunt.config('bsp.maven.destDir')) {
 			var pom = grunt.config('bsp.maven.pom');
-			var buildName;
-			var xml;
+
 			if (fs.existsSync(pom)) {
-				xml = parser.toJson( fs.readFileSync(pom), { object: true } );
-				buildName = xml.project.artifactId + '-' + xml.project.version;
-				grunt.config('bsp.maven.destDir', '<%= bsp.maven.targetDir %>/' + buildName);
-				grunt.log.writeln( grunt.config('bsp.maven.destDir') );
+                var done = this.async();
+
+				parseString(fs.readFileSync(pom), function (err, result) {
+                    var buildName = result.project.artifactId + '-' + result.project.version;
+
+                    grunt.config('bsp.maven.destDir', '<%= bsp.maven.targetDir %>/' + buildName);
+                    grunt.log.writeln(grunt.config('bsp.maven.destDir'));
+                    done();
+                });
+
 			} else {
 				grunt.fail.fatal(pom + ' not found, is required to configure the target directory');
 			}
